5b51745915a3eac0eb6c9762a6c356e6169966aa
Ceasar Barbosa <cbarbosa@solstice.com>
2019-02-27 09:45:15 -0600

    added methods for returning the hello world bean and using path variables

--- src/main/java/com/training/rest/webservices/springmicroservices/HelloWorldController.java
+++ src/main/java/com/training/rest/webservices/springmicroservices/HelloWorldController.java
@@ -1,14 +1,31 @@
 package com.training.rest.webservices.springmicroservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloWorldController {
 
-     @GetMapping(path = "/hello-world")
-//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String getHelloWorld() {
-        return "Woooo!";
+        return "Hello World.";
+    }
+
+
+    // Returning a Bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean getHelloWorldBean() {
+        return new HelloWorldBean("Hello World!!!");
+    }
+
+    // Using path variables
+    @GetMapping(path = "/hello-world-bean/{name}")
+    public HelloWorldBean getHelloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
